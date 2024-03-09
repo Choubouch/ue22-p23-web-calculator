7d6b728c8e99d6058c0fa8ed5ae213c351c2e77e
Mathis Verdan <mathis.verdan@etu.minesparis.psl.eu>
2024-03-09 15:15:27 +0100

    calculatrice presque finie

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,67 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+window.addEventListener('load',
+  function () {
+    let list_bouttons = this.document.querySelectorAll("button")
+    let entree_utilisateur = ""
+    let affichage = ""
+    let liste_nombres = []
+    let liste_operateurs = []
+    for (let bouton of list_bouttons){
+        bouton.addEventListener('click', () => {
+            console.log(liste_nombres)
+            if(bouton.textContent == "AC"){
+                entree_utilisateur = ""
+                liste_nombres = []
+                liste_operateurs = []
+                affichage = "0"
+            }
+            else if(["+", "-", "÷", "×"].includes(bouton.textContent)){
+                liste_nombres.push(entree_utilisateur)
+                liste_operateurs.push(bouton.textContent)
+                entree_utilisateur = ""
+                affichage = bouton.textContent
+            }
+            else if(bouton.textContent == "="){
+                liste_nombres.push(entree_utilisateur)
+                entree_utilisateur = ""
+                resultat = Number(liste_nombres[0])
+                for(let i = 0; i < liste_operateurs.length ; i++){
+                  if(liste_operateurs[i] == "+"){
+                    resultat += Number(liste_nombres[i+1])
+                  }
+                  else if(liste_operateurs[i] == "-"){
+                    resultat -= Number(liste_nombres[i+1])
+                  }
+                  else if(liste_operateurs[i] == "×"){
+                    resultat *= Number(liste_nombres[i+1])
+                  }
+                  else if(liste_operateurs[i] == "÷"){
+                    resultat /= Number(liste_nombres[i+1])
+                  }
+                  
+                }
+                if(isNaN(resultat)){
+                  entree_utilisateur = ""
+                  liste_nombres = []
+                  liste_operateurs = []
+                  affichage = "Erreur de saisie"
+                 }
+                else{
+                  affichage = resultat
+                }
+                liste_nombres = []
+                liste_operateurs = []
+            }
+            else{
+              entree_utilisateur = entree_utilisateur + bouton.textContent
+              affichage = entree_utilisateur
+            }
+            
+            this.document.querySelector(".calculator__display").textContent = affichage
+            })
+    }
+    
+
+  })
